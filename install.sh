e92f50ff1d458944adbc66b7891e2f6aaed5ad12
Christian Dinh <ctdinh93@gmail.com>
2015-10-11 14:11:13 -0500

    Change i3 mod key to alt, clean up symlinks again

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/.dotfiles/    # dotfiles directory
+dir=~/.dotfiles     # dotfiles directory
 olddir=$dir/backup/ # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
@@ -29,5 +29,5 @@ for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
-    ln -s $dir$file ~/.$file
+    ln -s $dir/$file ~/.$file
 done
