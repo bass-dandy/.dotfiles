d5f7f24823d388e3abc643214721dd4ca5cc484e
Christian Dinh <christian.dinh@civitaslearning.com>
2016-06-18 03:09:05 -0500

    add tmux conf

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ dir=~/.dotfiles     # dotfiles directory
 olddir=$dir/backup/ # old dotfiles backup directory
 
 # list of files/folders to symlink in homedir
-files="bash_aliases profile gitconfig vim vimrc powerline-shell" 
+files="bash_aliases profile gitconfig vim vimrc powerline-shell tmux.conf" 
 
 ##########
 
